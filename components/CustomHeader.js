49f53aa6ac07ce7c64d5b2a9b4c39da0d63f8cf1
Guillaume Benard <guillaume.benard0@gmail.com>
2023-05-24 21:04:16 +0200

    goback arrow

--- components/CustomHeader.js
+++ components/CustomHeader.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { globalStyles } from '../styles/global'
-import { Colors } from '../styles/colors'
-
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { globalStyles } from '../styles/global';
+import { Colors } from '../styles/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 const CustomHeader = () => {
+  const navigation = useNavigation();
+
+  if (navigation && navigation.canGoBack()) {
     return (
-      <View style={styles.header}>
-      <Image style={styles.image} source= {require('../assets/logo/logo.png')}></Image>
+      <View style={styles.goBackHeader}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.arrow}>
+          <Icon name="arrow-back" size={30} color={Colors.primaryPurple} />
+        </TouchableOpacity>
+        <Image style={styles.image} source={require('../assets/logo/logo.png')} />
       </View>
     );
-  };
+  }
+  
+  return (
+    <View style={styles.header}>
+      <Image style={styles.image} source={require('../assets/logo/logo.png')} />
+    </View>
+  );
+};
 
-export default CustomHeader
+export default CustomHeader;
 
 const styles = StyleSheet.create({
   header: {
@@ -22,14 +36,28 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     borderBottomWidth: 1,
     borderBottomColor: Colors.primaryPurple,
-    paddingBottom: 15
+    paddingBottom: 15,
+  },
+  goBackHeader: {
+    flexDirection: 'row',
+    backgroundColor: Colors.primaryDark,
+    height: 120,
+    alignItems: 'flex-end',
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.primaryPurple,
+    paddingLeft: 15,
+    paddingRight: 15,
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
   },
-  image:{
+  image: {
     width: 50,
-    height: 50
+    height: 50,
+  },
+  arrow:{
+    width: '43%'
   }
 });
