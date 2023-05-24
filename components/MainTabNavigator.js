92860369fd3b71e75eb2f826405d757e94ec2ee0
Guillaume Benard <guillaume.benard0@gmail.com>
2023-05-24 20:18:49 +0200

    mainTab ajout icon + couleur on focused

--- components/MainTabNavigator.js
+++ components/MainTabNavigator.js
@@ -3,14 +3,16 @@ import TicketScreen from '../screens/logged/TicketScreen';
 import SearchScreen from '../screens/logged/SearchScreen';
 import { Colors } from '../styles/colors';
 import MyTickets from '../screens/logged/MyTickets';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { globalStyles } from '../styles/global';
 
 const Tab = createBottomTabNavigator();
 
 function MainTabNavigator() {
-    return (
-      <Tab.Navigator 
+  return (
+    <Tab.Navigator 
       initialRouteName='MainTab'
-      screenOptions={({route}) => ({
+      screenOptions={({ route }) => ({
         tabBarStyle: {
           position: 'absolute',
           marginBottom: 25,
@@ -21,20 +23,46 @@ function MainTabNavigator() {
           borderTopWidth: 0,
           backgroundColor: Colors.secondaryDark
         },
-        tabBarLabelStyle:{
+        tabBarLabelStyle: {
           alignItems: 'flex-end',
           justifyContent: 'flex-end',
-        }
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
+          if (route.name === 'Mes Billets') {
+            iconName = focused ? 'ticket' : 'ticket'; // Change the icon name and color based on focus
+          } else if (route.name === 'Fil d\'actu') {
+            iconName = focused ? 'newspaper-o' : 'newspaper-o'; // Change the icon name and color based on focus
+          } else if (route.name === 'Profil') {
+            iconName = focused ? 'user-o' : 'user-o'; // Change the icon name and color based on focus
+          }
+
+          // Return the custom icon component
+          return <Icon style={globalStyles.buttonIcon} name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: Colors.primaryPurple,
+        tabBarInactiveTintColor: 'grey',
+        
       })}
-      >
-        <Tab.Screen name="Mes Billets" options={{ headerShown : false}} component={MyTickets} />
-        <Tab.Screen name="Fil d'actu" options={{ headerShown : false}} component={SearchScreen} />
-        <Tab.Screen name="Profil" options={{ headerShown : false}} component={TicketScreen} />
-        {/* rechanger le sens apres custom tabbar */}
-      </Tab.Navigator>
-    );
-  }
-
+    >
+      <Tab.Screen
+        name="Mes Billets"
+        options={{ headerShown: false }}
+        component={MyTickets}
+      />
+      <Tab.Screen
+        name="Fil d'actu"
+        options={{ headerShown: false }}
+        component={SearchScreen}
+      />
+      <Tab.Screen
+        name="Profil"
+        options={{ headerShown: false }}
+        component={TicketScreen}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default MainTabNavigator;
--- styles/colors.js
+++ styles/colors.js
@@ -1,16 +1,15 @@
 export const Colors = {
+    transparent: 'rgba(0, 0, 0, 0)',
     primaryDark: '#1C1B1D',
     secondaryDark: '#2a282b',
-
     primaryLight: '#E8E0EB',
     secondaryLight: '#fcf5ff',
     dimmedLight: '#69636b',
-
     primaryPurple: '#8f00ff',
     secondaryPurple: '#520191',
-
-    // colors variants
-
     postColor: '#2a282b',
-    postSecondaryColor: '#606060'
-}
+    postSecondaryColor: '#606060',
+  };
+  
+  export const radialGradient = `radial-gradient(${Colors.primaryDark}, ${Colors.secondaryPurple})`;
+  
